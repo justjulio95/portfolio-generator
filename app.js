1dd7fc6c854dbd4a3bb9799f2ea2e7467f0c85de
Julio.S <just.julio95@gmail.com>
2022-04-30 12:31:15 -0400

    generated basic HTML with app.js

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// pulls information from the command line, beginning from index position 2
-const profileDataArgs = process.argv.slice(2, process.argv.length)
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-//NOTE: With arrow functions, you don't need to code 'return' if the function is just handling one task
+const profileDataArgs = process.argv.slice(2)
 
-const printProfileData = profileDataArr => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i+=1){
-        console.log(profileDataArr[i])
-    }
+const [name, github] = profileDataArgs
 
-    console.log('===================')
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
 
-    // Is the same as this...
-
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-};
-
-printProfileData(profileDataArgs)
+    console.log('Portfolio complete! Checkout index.html to see the output');
+});
