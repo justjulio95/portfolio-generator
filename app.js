a41082152fbbd6d43f66b925580f8f34d459cc4f
Julio.S <just.julio95@gmail.com>
2022-05-03 13:17:36 -0400

    generated html for the page dynamically according to users projects and 'featured' status

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer')
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -135,7 +136,13 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Checkout index.html in this directory to see it!');
+    });
 })
 
 //const pageHTML = generatePage(name, github)
